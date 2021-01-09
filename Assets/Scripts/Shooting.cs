9ff2e919b3a68d7eeaa42af7f86bb72e0ca7c5f7
GrantMatteo <matteoboy3@gmail.com>
2021-01-08 20:54:56 -0500

    merging forrest changes

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float bulletSpeed = 10;
+    public GameObject bullet;
+    public Transform playerTransform;
+
+
+    bool hasBullet = true; //do we have the bullet on us?
+    private GameObject bulletInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && hasBullet)
+        {
+            fireTowardsMouse();
+            hasBullet = false;
+        }
+    }
+
+    void fireTowardsMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 velDirection = mousePos - new Vector2(playerTransform.position.x, playerTransform.position.y);
+        velDirection.Normalize();
+        bulletInstance = Instantiate(bullet, playerTransform.position, playerTransform.rotation);
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = velDirection * bulletSpeed;
+    }
+}
