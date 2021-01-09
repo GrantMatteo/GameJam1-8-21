27718fae81bbf5edb7d8d34cd823728cd0f8620b
GrantMatteo <matteoboy3@gmail.com>
2021-01-08 23:17:21 -0500

    added stupid workaround to make bullets work

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,19 @@ public class Enemy : MonoBehaviour {
     public float health = 10f;
 
     //we'll probably need some Collision events with bullets to increase score and stuff
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision) {
 
+        if (collision.gameObject.tag == "Bullet")
+        {
+            //TODO increase score
+            Death();
+        }
     }
-
     private void OnCollisionExit2D(Collision2D collision) {
 
+    }
+    public void Death()
+    {
+        Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    
+    public GameObject bulletTrigger;
+
+    private GameObject child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        child = this.gameObject.GetComponent<Transform>().GetChild(0).gameObject;
+        child.SendMessage("SetParent", this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //we'll probably need some Collision events with bullets to increase score and stuff
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+}
