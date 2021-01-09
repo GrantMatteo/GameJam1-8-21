626e63848bc97aa39e92793f84122baed56c0630
Forrestino <jibjab910@gmail.com>
2021-01-08 20:14:28 -0500

    Added Menu functionality

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SetText(string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+    }
+    public void LoadLevel(string Level)
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Untitled");
+    }
+}
---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Untitled");
+    }
+}
