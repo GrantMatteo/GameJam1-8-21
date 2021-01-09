f40eb646359896f29c761510e0775fcbb5757e25
GrantMatteo <matteoboy3@gmail.com>
2021-01-09 03:35:05 -0500

    heaefjjefijafewoijaweofijawoifjaoiwejf

--- Assets/Scripts/EnemyMovement/FollowPlayer.cs
+++ Assets/Scripts/EnemyMovement/FollowPlayer.cs
@@ -15,7 +15,7 @@ public class FollowPlayer : MonoBehaviour {
     }
     private void Update() {
         Vector2 towardPlayer = player.transform.position - transform.position;
-        Vector2 steering = towardPlayer.normalized - rb.velocity.normalized;
+        Vector2 steering = towardPlayer.normalized;
         Vector2 force = steering * followStrength * Time.deltaTime;
         rb.AddForce(force);
 //        if (rb.velocity.magnitude > maxSpeed) {
