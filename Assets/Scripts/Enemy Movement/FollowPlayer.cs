5a49daccb4b78ac7908bd6e825a59bc322b3aa3f
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2021-01-08 20:59:33 -0500

    more scripts

---
+++ Assets/Scripts/Enemy Movement/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FollowPlayer : MonoBehaviour {
+    private GameObject player;
+    public float followStrength = 5f;
+    private void Awake() {
+        player = GameObject.Find("Player");
+    }
+    private void Update() {
+        Vector2 towardPlayer = (player.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody2D>().AddForce(towardPlayer * followStrength * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    //we'll probably need some Collision events with bullets to increase score and stuff
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+
+    }
+}
