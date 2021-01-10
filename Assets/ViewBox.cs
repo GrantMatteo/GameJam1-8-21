0badb5485f0eacd0f91823d51fc8d57ff5a4a01e
GrantMatteo <matteoboy3@gmail.com>
2021-01-10 00:46:31 -0500

    combined new animations with viewport tech

--- Assets/ViewBox.cs
+++ Assets/ViewBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ViewBox : MonoBehaviour
 {
-    int inCount = 0;
+    public int inCount = 0;
     // Start is called before the first frame update
     void Start()
     {
