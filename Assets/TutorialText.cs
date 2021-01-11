15c432754157a152d3b90d88bbce4d38d882e712
GrantMatteo <matteoboy3@gmail.com>
2021-01-11 13:07:31 -0500

    finishing touches, added functionality

--- Assets/TutorialText.cs
+++ Assets/TutorialText.cs
@@ -11,7 +11,7 @@ public class TutorialText : MonoBehaviour
     }
 
     int curProgress = 0;
-    string[] messages = { "WASD to move", "Left Click to Shoot", "Right Click to Teleport", "Weapons Insufficient. Artifacts: 0/3", "Teleback Unlocked. (Right Click)", "Weapons Insufficient. Artifacts: 1/3", "Charge Shot Unlocked (hold left click)", "Weapons Insufficient. Artifacts: 2/3", "TeleStun Unlocked. Good luck."};
+    string[] messages = { "WASD to move", "Left Click to Shoot", "Right Click to Teleport", "Weapons Insufficient. Artifacts: 0/3", "Teleback Unlocked. (Right Click)", "Weapons Insufficient. Artifacts: 1/3", "Charge Shot Unlocked (hold left click)", "Weapons Insufficient. Artifacts: 2/3", "TeleStun Unlocked. Retrieve Core Rune.", "Kill."};
     Text textbox;
 
     public void pushToMin(int x)
